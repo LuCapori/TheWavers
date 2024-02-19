dd556467d7548384007ee03e2f1b1ce4ec2f8867
LuCapori <l.caporicci95@gmail.com>
2024-02-19 11:01:58 +0100

    First Commit

---
+++ TheWavers/TheWaversApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWaversApp.swift
+//  TheWavers
+//
+//  Created by Luca Caporicci on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TheWaversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
