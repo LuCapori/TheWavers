aed5440bc3eca65bf3c0d5d526efd81dbe72e02f
LuCapori <l.caporicci95@gmail.com>
2024-02-27 12:21:32 +0100

    TExtInser, ViewChange e Nuova page di sintomi

--- TheWavers/SyntomsNew.swift
+++ TheWavers/SyntomsNew.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct SyntomsNew: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
+        //  GeometryReader  {reader in
+        HStack{
+            VStack(alignment: .leading){
+                
+                
+                Text("What are your main \(Text("symptoms?").foregroundColor(Color.testo1))")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white) // Colore normale
+                    .font(.system(size: 35))
+                    
+                
+                //                     .offset(x: -30, y:-300)
+                
+                Text("Type of pain")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                //                    .offset(x: -110, y:-260)
+            }
+            
+        }
         
     }
 }
--- TheWavers/TextInser.swift
+++ TheWavers/TextInser.swift
@@ -13,27 +13,26 @@ struct TextInser: View {
     @State private var description: String = ""
     
     var body: some View {
-                VStack{
-                    TextField("Describe how you are feeling right now...", text: $description)
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                        .frame(maxWidth: .infinity)
-                        .opacity(value == 0 ? 1 : 0)
-                    
-                        .toolbar {
-                            ToolbarItem(placement: .keyboard) {
-                                Button(action: {
-                                    // Qui puoi gestire l'azione del pulsante (es. passaggio alla schermata successiva)
-                                    // Per ora, puoi solo nascondere la tastiera
-                                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                                }) {
-                                    Image(systemName: "arrow.right.circle.fill")
-                                }
-                            }
+        VStack{
+            TextField("Describe how you are feeling right now...", text: $description)
+                .font(.title3)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+//                .opacity(value == 0 ? 1 : 0)
+                .keyboardType(.default)
+            
+                .toolbar {
+                    ToolbarItem(placement: .keyboard) {
+                        Button(action: {
+                            // Qui puoi gestire l'azione del pulsante (es. passaggio alla schermata successiva)
+                            // Per ora, puoi solo nascondere la tastiera
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }) {
+                            Image(systemName: "arrow.right.circle.fill")
                         }
+                    }
+                }
         }
-        
-        
     }
 }
 
