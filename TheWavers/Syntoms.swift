054206800c3c84941a378d8610a8f480a7c72b96
LuCapori <l.caporicci95@gmail.com>
2024-02-27 10:42:32 +0100

    New app icon + syntoms and them views

--- TheWavers/Syntoms.swift
+++ TheWavers/Syntoms.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import UIKit
 
+
 class ViewController: UIViewController {
     
     @State var dimensioni : String = "All"
---
+++ TheWavers/SyntomsNew.swift
@@ -0,0 +1,20 @@
+//
+//  SyntomsNew.swift
+//  TheWavers
+//
+//  Created by Luca Caporicci on 27/02/24.
+//
+
+import SwiftUI
+
+struct SyntomsNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+    }
+}
+
+#Preview {
+    SyntomsNew()
+}
