dd9cb85430a9f8c3a7d018ac7e18ca56121821d1
LuCapori <l.caporicci95@gmail.com>
2024-03-04 11:01:42 +0100

    Finished buttons and other little changes

--- TheWavers/SyntomsNew.swift
+++ TheWavers/SyntomsNew.swift
@@ -12,11 +12,18 @@ struct SyntomsNew: View {
     
     let symptoms = [GridItem(.flexible()), GridItem(.flexible()),GridItem(.flexible())]
     @State var sintomi : [SintomiFile] = sintomiData
-    
     @State var selectedSymptoms: [SintomiFile] = []
     
+    let pains = [GridItem(.flexible()), GridItem(.flexible()),GridItem(.flexible())]
+    @State var pain : [PainPosition] = painData
+    @State var selectedPains: [PainPosition] = []
+    
+    let causes = [GridItem(.flexible()), GridItem(.flexible()),GridItem(.flexible())]
+    @State var cause : [CausesTrig] = causesData
+    @State var selectedCauses: [CausesTrig] = []
+    
     var body: some View {
-        //  GeometryReader  {reader in
+        
         
         ZStack{
             Image("background1")
@@ -33,7 +40,7 @@ struct SyntomsNew: View {
                     
                     Spacer(minLength: 45)
                     
-                    Text("Type of pain")
+                    Text("Type of pain")  //prima lista
                         .offset(CGSize(width: 10.0, height: 10.0))
                         .font(.title3)
                         .bold()
@@ -71,10 +78,90 @@ struct SyntomsNew: View {
                             }
                         }
                     }
-                    )}
+                    )
+                    .padding(.bottom, 30)
+                    
+                    Text("Where do you feel it more?")   //seconda lista
+                    
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 20)
+                    
+                    LazyVGrid(columns: pains, content: {
+                        ForEach(pain) { painPosition in
+                            Button(action: {
+                                if selectedPains.contains(where: { $0.id == painPosition.id }) {
+                                    selectedPains.removeAll(where: { $0.id == painPosition.id })
+                                } else {
+                                    selectedPains.append(painPosition)
+                                }
+                                
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .foregroundColor(selectedPains.contains(where: { $0.id == painPosition.id }) ? .selectedItem : Color.clear)
+                                        .frame( width: 110, height: 39)
+                                    // Contenuto del pulsante
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 100)
+                                                .stroke(selectedPains.contains(where: { $0.id == painPosition.id }) ? .black : .white, lineWidth: 0.4)
+                                            
+                                        )
+                                    
+                                    Text(painPosition.title)
+                                        .foregroundColor(selectedPains.contains(where: { $0.id == painPosition.id }) ? .white : .white)
+                                        .padding()
+                                        .background(Color.clear)
+                                        .cornerRadius(10)
+                                    
+                                }
+                            }
+                        }
+                    }
+                    )
+                    .padding(.bottom, 30)
+                    
+                    Text("Causes and triggers")  //terza lista
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 20)
+                    
+                    LazyVGrid(columns: causes, content: {
+                        ForEach(cause) { causesTrig in
+                            Button(action: {
+                                if selectedCauses.contains(where: { $0.id == causesTrig.id }) {
+                                    selectedCauses.removeAll(where: { $0.id == causesTrig.id })
+                                } else {
+                                    selectedCauses.append(causesTrig)
+                                }
+                                
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .foregroundColor(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .selectedItem : Color.clear)
+                                        .frame( width: 110, height: 39)
+                                    // Contenuto del pulsante
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 100)
+                                                .stroke(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .black : .white, lineWidth: 0.4)
+                                            
+                                        )
+                                    
+                                    Text(causesTrig.title)
+                                        .foregroundColor(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .white : .white)
+                                        .padding()
+                                        .background(Color.clear)
+                                        .cornerRadius(10)
+                                    
+                                }
+                            }
+                        }
+                    }
+                    )
+                }
             }
-            
-            
         }
     }
 }
