aa9b9cddb6d055449ad9d36487f1b55fcaa7def3
LuCapori <l.caporicci95@gmail.com>
2024-03-06 12:16:39 +0100

    Added feedback and somethig

--- TheWavers/SyntomsNew.swift
+++ TheWavers/SyntomsNew.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct SyntomsNew: View {
     
@@ -22,179 +23,184 @@ struct SyntomsNew: View {
     @State var cause : [CausesTrig] = causesData
     @State var selectedCauses: [CausesTrig] = []
     
+    let impactFeedback = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-
-            ZStack{
-                Image("background1")
-                    .resizable()
-                
-                ScrollView{
-                    VStack(alignment: .leading){
-//                        VStack {
-//                            Spacer()
-//                            HStack {
-//                                Spacer()
-//                                NavigationLink(destination: TextInser()) {
-//                                    Text("GO ON")
-//                                        .foregroundColor(.white)
-//                                        .font(.title3)
-//                                        .fontWeight(.heavy)
-//                                        .background(Color.clear)
-//                                        .cornerRadius(8)
-//                                }
-//                                .padding(.horizontal, 20)
-//                            }
-//                        }
-
-                        
-                        Text("What are your main \(Text("symptoms?").foregroundColor(Color.testo))")
-                            .fontWeight(.heavy)
-                            .foregroundColor(.white) // Colore normale
-                            .font(.system(size: 35))
-                            .offset(CGSize(width: 10.0, height: 10.0))
-                        
-                        Spacer(minLength: 45)
-                        
-                        Text("Type of pain")  //prima lista
-                            .offset(CGSize(width: 10.0, height: 10.0))
-                            .font(.title3)
-                            .bold()
-                            .padding(.bottom, 20)
-                        
-                        
-                        LazyVGrid(columns: symptoms, content: {
-                            ForEach(sintomi) { sintomiFile in
-                                Button(action: {
-                                    if selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) {
-                                        selectedSymptoms.removeAll(where: { $0.id == sintomiFile.id })
-                                    } else {
-                                        selectedSymptoms.append(sintomiFile)
-                                    }
-                                    
-                                }) {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 100)
-                                            .foregroundColor(selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) ? .selectedItem : Color.clear)
-                                            .frame( width: 110, height: 39)
-                                        // Contenuto del pulsante
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 100)
-                                                    .stroke(selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) ? .black : .white, lineWidth: 0.4)
-                                                
-                                            )
-                                        
-                                        Text(sintomiFile.title)
-                                            .foregroundColor(selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) ? .white : .white)
-                                            .padding()
-                                            .background(Color.clear)
-                                            .cornerRadius(10)
-                                        
-                                    }
+        
+        ZStack{
+            Image("background1")
+                .resizable()
+            
+            ScrollView{
+                VStack(alignment: .leading){
+                    //                        VStack {
+                    //                            Spacer()
+                    //                            HStack {
+                    //                                Spacer()
+                    //                                NavigationLink(destination: TextInser()) {
+                    //                                    Text("GO ON")
+                    //                                        .foregroundColor(.white)
+                    //                                        .font(.title3)
+                    //                                        .fontWeight(.heavy)
+                    //                                        .background(Color.clear)
+                    //                                        .cornerRadius(8)
+                    //                                }
+                    //                                .padding(.horizontal, 20)
+                    //                            }
+                    //                        }
+                    
+                    
+                    Text("What are your main \(Text("symptoms?").foregroundColor(Color.testo))")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white) // Colore normale
+                        .font(.system(size: 35))
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                    
+                    Spacer(minLength: 45)
+                    
+                    Text("Type of pain")  //prima lista
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 20)
+                    
+                    
+                    LazyVGrid(columns: symptoms, content: {
+                        ForEach(sintomi) { sintomiFile in
+                            Button(action: {
+                                if selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) {
+                                    selectedSymptoms.removeAll(where: { $0.id == sintomiFile.id })
+                                } else {
+                                    selectedSymptoms.append(sintomiFile)
                                 }
-                            }
-                        }
-                        )
-                        .padding(.bottom, 30)
-                        
-                        Text("Where do you feel it more?")   //seconda lista
-                        
-                            .offset(CGSize(width: 10.0, height: 10.0))
-                            .font(.title3)
-                            .bold()
-                            .padding(.bottom, 20)
-                        
-                        LazyVGrid(columns: pains, content: {
-                            ForEach(pain) { painPosition in
-                                Button(action: {
-                                    if selectedPains.contains(where: { $0.id == painPosition.id }) {
-                                        selectedPains.removeAll(where: { $0.id == painPosition.id })
-                                    } else {
-                                        selectedPains.append(painPosition)
-                                    }
-                                    
-                                }) {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 100)
-                                            .foregroundColor(selectedPains.contains(where: { $0.id == painPosition.id }) ? .selectedItem : Color.clear)
-                                            .frame( width: 110, height: 39)
-                                        // Contenuto del pulsante
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 100)
-                                                    .stroke(selectedPains.contains(where: { $0.id == painPosition.id }) ? .black : .white, lineWidth: 0.4)
-                                                
-                                            )
-                                        
-                                        Text(painPosition.title)
-                                            .foregroundColor(selectedPains.contains(where: { $0.id == painPosition.id }) ? .white : .white)
-                                            .padding()
-                                            .background(Color.clear)
-                                            .cornerRadius(10)
-                                        
-                                    }
-                                }
-                            }
-                        }
-                        )
-                        .padding(.bottom, 30)
-                        
-                        Text("Causes and triggers")  //terza lista
-                            .offset(CGSize(width: 10.0, height: 10.0))
-                            .font(.title3)
-                            .bold()
-                            .padding(.bottom, 20)
-                        
-                        LazyVGrid(columns: causes, content: {
-                            ForEach(cause) { causesTrig in
-                                Button(action: {
-                                    if selectedCauses.contains(where: { $0.id == causesTrig.id }) {
-                                        selectedCauses.removeAll(where: { $0.id == causesTrig.id })
-                                    } else {
-                                        selectedCauses.append(causesTrig)
-                                    }
-                                    
-                                }) {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 100)
-                                            .foregroundColor(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .selectedItem : Color.clear)
-                                            .frame( width: 110, height: 39)
-                                        // Contenuto del pulsante
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 100)
-                                                    .stroke(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .black : .white, lineWidth: 0.4)
-                                                
-                                            )
-                                        
-                                        Text(causesTrig.title)
-                                            .foregroundColor(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .white : .white)
-                                            .padding()
-                                            .background(Color.clear)
-                                            .cornerRadius(10)
-                                        
-                                    }
-                                }
-                            }
-                        }
-                        )
-                    }
-                }
-                
-                ZStack {
-                    GeometryReader { reader in
-                        NavigationLink(destination: TextInser()) {
-                            Text("GO ON")
-                                .foregroundColor(.white)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                                .background(Color.clear)
-                                .cornerRadius(8)
+                                impactFeedback.impactOccurred()
                                 
-                        }.frame(width: reader.size.width, alignment: .trailing)
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .foregroundColor(selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) ? .selectedItem : Color.clear)
+                                        .frame( width: 110, height: 39)
+                                    // Contenuto del pulsante
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 100)
+                                                .stroke(selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) ? .black : .white, lineWidth: 0.4)
+                                            
+                                        )
+                                    
+                                    Text(sintomiFile.title)
+                                        .foregroundColor(selectedSymptoms.contains(where: { $0.id == sintomiFile.id }) ? .white : .white)
+                                        .padding()
+                                        .background(Color.clear)
+                                        .cornerRadius(10)
+                                    
+                                }
+                            }
+                        }
                     }
+                    )
+                    .padding(.bottom, 30)
+                    
+                    Text("Where do you feel it more?")   //seconda lista
+                    
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 20)
+                    
+                    LazyVGrid(columns: pains, content: {
+                        ForEach(pain) { painPosition in
+                            Button(action: {
+                                if selectedPains.contains(where: { $0.id == painPosition.id }) {
+                                    selectedPains.removeAll(where: { $0.id == painPosition.id })
+                                } else {
+                                    selectedPains.append(painPosition)
+                                }
+                                impactFeedback.impactOccurred()
+                                
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .foregroundColor(selectedPains.contains(where: { $0.id == painPosition.id }) ? .selectedItem : Color.clear)
+                                        .frame( width: 110, height: 39)
+                                    // Contenuto del pulsante
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 100)
+                                                .stroke(selectedPains.contains(where: { $0.id == painPosition.id }) ? .black : .white, lineWidth: 0.4)
+                                            
+                                        )
+                                    
+                                    Text(painPosition.title)
+                                        .foregroundColor(selectedPains.contains(where: { $0.id == painPosition.id }) ? .white : .white)
+                                        .padding()
+                                        .background(Color.clear)
+                                        .cornerRadius(10)
+                                    
+                                }
+                            }
+                        }
+                    }
+                    )
+                    .padding(.bottom, 30)
+                    
+                    Text("Causes and triggers")  //terza lista
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 20)
+                    
+                    LazyVGrid(columns: causes, content: {
+                        ForEach(cause) { causesTrig in
+                            Button(action: {
+                                if selectedCauses.contains(where: { $0.id == causesTrig.id }) {
+                                    selectedCauses.removeAll(where: { $0.id == causesTrig.id })
+                                } else {
+                                    selectedCauses.append(causesTrig)
+                                }
+                                impactFeedback.impactOccurred()
+                                
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .foregroundColor(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .selectedItem : Color.clear)
+                                        .frame( width: 110, height: 39)
+                                    // Contenuto del pulsante
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 100)
+                                                .stroke(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .black : .white, lineWidth: 0.4)
+                                            
+                                        )
+                                    
+                                    Text(causesTrig.title)
+                                        .foregroundColor(selectedCauses.contains(where: { $0.id == causesTrig.id }) ? .white : .white)
+                                        .padding()
+                                        .background(Color.clear)
+                                        .cornerRadius(10)
+                                    
+                                }
+                            }
+                        }
+                    }
+                    )
                 }
-
             }
+            
+            ZStack {
+                GeometryReader { reader in
+                    NavigationLink(destination: TextInser()) {
+                        Text("GO ON")
+                            .foregroundColor(.white)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                            .background(Color.clear)
+                            .cornerRadius(8)
+                        
+                    }.frame(width: reader.size.width, alignment: .trailing)
+                }
+            }
+            
         }
     }
+}
 
 
 
